333e50ebd80d10ccec2678f8f4f8240b7abeb03c
Vincent Guigui <vguigui@octo.com>
2020-09-08 11:20:11 +0200

    Adding documentation and source code

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# opencv-python-camera-settings
-Here is the shortest python project to change your camera settings (focus, exposure, contrast, brightness) using OpenCV
+# Set your Camera settings using Python and OpenCV
+Here is the shortest python project to change your camera settings (focus, exposure, contrast, brightness) using OpenCV.
+It will let you play with all the values to find the right one for your camera model.
+
+**Note:** 
+- Based on OpenCV implementation and the camera/webcam driver for your operating system, some settings (called Capabilities) may not be available and adjustable.
+- Settings values (direction and range) are highly dependant of the camera manufacturer and model. \
+  So you will have to use a trial/error strategy to find the right values for your camera.
+  - ie: Higher brightness value could mean brigher or darker image on another camera.
+  - ie: contrast=0 could be mean 'normal' contrast or 'very low' contrast on another camera.
+
+# Installation 
+```
+pip install opencv-python==4.4.0.40
+```
+
+#Usage
+
+Start ```opencv-camera_settings.py```
+All settings values are shown is the console for you to reuse in other OpenCV project.
+
+## Keyboard shortcuts
+| Shortcut | Action |
+| :------: | ------ | 
+|    B     | Increase brightness |
+|    b     | Decrease brightness |
+|    E     | Increase exposure |
+|    e     | Decrease exposure |
+|    C     | Increase contrast |
+|    c     | Decrease contrast |
+|    F     | Increase focus |
+|    f     | Decrease focus |
+|  0 - N   | Switch camera (0 is the default main camera) |
+|    L     | Enable live view (Camera will be reserved by this script) |
+|    l     | Disable live view (Camera will be usable by other apps while still allowing settings) |
+
---
+++ opencv-camera-settings.py
@@ -0,0 +1,93 @@
+import cv2
+import numpy as np
+
+#-2 (bright) -11(dark)
+exposure=-5
+
+#-130 (dark) +130(bright)
+brightness=0
+
+#-130 (dark) +130(bright)
+contrast=0
+
+#0 - 500
+focus=0
+
+#0 to N (camera index, 0 is the default OS main camera)
+camera_id=0
+
+live_feed=True
+
+vid = cv2.VideoCapture(camera_id)
+if not vid.isOpened():
+	raise ValueError('Unable to open video source')
+blank_image = np.zeros((200,200,3), np.uint8)
+vid.set(cv2.CAP_PROP_EXPOSURE, exposure)
+vid.set(cv2.CAP_PROP_BRIGHTNESS, brightness)
+
+while(True):
+	if live_feed:
+		_, frame = vid.read()
+		if frame is not None:
+			cv2.imshow('image',frame)
+	else:
+		cv2.imshow('image',blank_image)
+		frame = None
+		
+	key = cv2.waitKey(5)
+	if key == ord('q'):
+		break
+	if key == ord('L'):
+		print(f'hide live video camera')
+		vid.release()
+		vid = cv2.VideoCapture(camera_id)
+		live_feed=False
+	if key == ord('l'):
+		print(f'show live video camera (blocked for other processes)')
+		vid = cv2.VideoCapture(camera_id)
+		live_feed=True
+	if key == ord('E'):
+		exposure+=0.5
+		r=vid.set(cv2.CAP_PROP_EXPOSURE, exposure)
+		print(f'exposure: {exposure}')
+	if key == ord('e'):
+		exposure-=0.5
+		print(f'exposure: {exposure}')
+		r=vid.set(cv2.CAP_PROP_EXPOSURE, exposure)
+	if key == ord('B'):
+		brightness+=10
+		print(f'brightness: {brightness}')
+		r=vid.set(cv2.CAP_PROP_BRIGHTNESS, brightness)
+	if key == ord('b'):
+		brightness-=10
+		print(f'brightness: {brightness}')
+		vid.set(cv2.CAP_PROP_BRIGHTNESS, brightness)
+	if key == ord('C'):
+		contrast+=10
+		print(f'contrast: {contrast}')
+		vid.set(cv2.CAP_PROP_CONTRAST, contrast)
+	if key == ord('c'):
+		contrast-=10
+		print(f'contrast: {contrast}')
+		vid.set(cv2.CAP_PROP_CONTRAST, contrast)
+	if key == ord('F'):
+		focus+=5
+		print(f'focus: {focus}')
+		vid.set(cv2.CAP_PROP_AUTOFOCUS, 0)
+		vid.set(cv2.CAP_PROP_FOCUS, focus)
+	if key == ord('f'):
+		focus-=5
+		print(f'focus: {focus}')
+		vid.set(cv2.CAP_PROP_AUTOFOCUS, 0)
+		vid.set(cv2.CAP_PROP_FOCUS, focus)
+	if key >= ord('0') and key <= ord('3'):
+		vid.release()
+		camera_id=key-ord('0')
+		vid = cv2.VideoCapture(camera_id)
+		if not vid.isOpened():
+			raise ValueError('Unable to open video source')
+		
+		
+if vid.isOpened():
+	vid.release()
+cv2.destroyAllWindows()
